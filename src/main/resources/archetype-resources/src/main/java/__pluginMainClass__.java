2e5e45edb23b7b30dbd2f19d7965ca1abfe557f6
Sycholic <c_chekay@yahoo.com>
2016-04-22 04:21:40 -0400

    getting there..

--- src/main/resources/archetype-resources/src/main/java/__pluginMainClass__.java
+++ src/main/resources/archetype-resources/src/main/java/__pluginMainClass__.java
@@ -3,6 +3,6 @@ package ${package};
 import java.util.logging.Level;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class ${pluginMainClass} extends JavaPlugin {
+public class ${mainClass} extends JavaPlugin {
 
 }
