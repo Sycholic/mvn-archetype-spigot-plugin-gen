237a4c7ee03c4133eb158547540138ec88970497
Sycholic <c_chekay@yahoo.com>
2016-04-24 00:55:57 -0400

    trying using unique variable names again now that hopefully file structure is valid now.

--- src/main/resources/archetype-resources/__artifactId__.java
+++ src/main/resources/archetype-resources/__artifactId__.java
@@ -1,8 +1,6 @@
 package ${package};
 
-import java.util.logging.Level;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class ${artifactId} extends JavaPlugin {
-
+public class $ {MainPluginClass} extends JavaPlugin {
 }
